a9a046516b660cf7a33ba5d7a82b5d31fab5e421
FlYz7 <114088988+FlYz7@users.noreply.github.com>
2022-09-25 19:31:27 -0300

    main(problema para criar sessoes)

--- src/App.java
+++ src/App.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-
-        
+        Integer[] sessoes;
+        Cinema cinema = new Cinema("Salvador" , "Bahia");
+        cinema.addFilmeCartaz(2);
+        cinema.addSalas(3); 
+        //cinema.createSessao(1, sessoes, 1); 
+        //cinema.createSessao(2, sessoes, 2);
+        //cinema.createSessao(3, sessoes, 2);
+        cinema.vendaIngresso();
     }
 }
